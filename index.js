80d304ecb0a0b8dc793d0aff89355321b2b10aaf
nthannn <nthanan.sri@gmail.com>
2024-02-25 17:09:13 +0700

    update code

---
+++ index.js
@@ -0,0 +1,32 @@
+const session = require("express-session");
+const express = require("express");
+const cors = require("cors");
+const passportSetup = require("./passport");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const app = express();
+
+app.use(
+	session({
+	  secret: "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",
+	  resave: false,
+	  saveUninitialized: true,
+	})
+  );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use("/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("Server is running!");
+});
---
+++ server.js
@@ -0,0 +1,26 @@
+const Parent_api = require('./Parent-api');
+const Personnel_api = require('./Pesonnel-api');
+const Student_api = require('./Student-api');
+
+var express = require('express')
+var cors = require('cors')
+require('dotenv').config();
+
+// ต่อ database หรือทำสิ่งอื่น ๆ ที่ต้องการกับค่า config
+var app = express();
+app.use(express.json());
+
+app.use(cors())
+
+//use routes
+app.use(Parent_api);
+// app.use(Personnel_api);
+// app.use(Student_api);
+
+
+//ser port
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
